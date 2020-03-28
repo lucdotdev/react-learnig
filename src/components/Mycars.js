30759563242890abf1c82ecd9ace4a1efd2687b0
luc-dotcom <lucm.dotcom@gmail.com>
2020-03-28 17:00:30 +0200

    .

---
+++ src/components/Mycars.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class Mycars extends Component {
+
+  render() {
+      return (
+          <h1>
+              Hello react
+          </h1>
+      )
+  }
+}
+
+export default Mycars;
+
